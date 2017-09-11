c6b75b5c714d79fba21d3db4509da1db338f49bb
trainwreck8888 <31801375+trainwreck8888@users.noreply.github.com>
2017-09-11 17:55:05 -0500

    new repo for bounty
    bounty new settings

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x42, 0x51, 0x56, 0x40, 0x44, 0x3f, 0x4d, 0x4e, 0x44, 0x45, 0x00, 0x32, 0x30, 0x13, 0x01, 0x10 } };
+}
